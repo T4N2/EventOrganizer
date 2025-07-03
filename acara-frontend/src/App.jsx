a2dd40fae259213962c84951bdbfd9881c87515b
fhatoni <168197386+fhatoni@users.noreply.github.com>
2025-07-03 14:23:12 +0700

    Update Package Tailwinds Vite

--- acara-frontend/src/App.jsx
+++ acara-frontend/src/App.jsx
@@ -1,19 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 // src/App.jsx
 import AcaraList from "./components/AcaraList";
+import { Link } from "lucide-react";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "./components/ui/navigation-menu";
 
 function App() {
   return (
     <div className="max-w-xl mx-auto mt-8">
+      <NavigationMenu viewport={false}>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <a href="#tentang">Tentang</a>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
       <AcaraList />
     </div>
   );
 }
 
-export default App
+export default App;
--- acara-frontend/src/components/AcaraList.jsx
+++ acara-frontend/src/components/AcaraList.jsx
@@ -26,7 +26,7 @@ export default function AcaraList() {
 
   return (
     <div className="p-4 space-y-6">
-      <h1 className="text-2xl font-bold mb-4">Daftar Acara Event</h1>
+      <h1 className="text-2xl font-bold mb-4 text-blue-400">Daftar Acara Event</h1>
       <AcaraForm onSuccess={fetchData} />
       <div className="space-y-4">
         {acara.map((item) => (
